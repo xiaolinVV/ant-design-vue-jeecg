bf2fec5bcc445a47cbc5541769c5a5f78e45d23c
张少林 <1083065285@qq.com>
2022-09-28 23:15:07 +0800

    feat：动态表单测试代码

--- src/components/designForm/FormBuildPage.vue
+++ src/components/designForm/FormBuildPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <k-form-build ref="kfb" :value="jsonData" />
+    <k-form-build ref="kfb" :value="jsonData" @submit="submitData" />
   </div>
 </template>
 
@@ -9,19 +9,14 @@ import { getAction } from '@/api/manage'
 export default {
   name: 'FormBuildPage',
   props: {
-    code: String,
+    config: Object,
   },
   data () {
     return {
       jsonData: "",
       url: {
-        loadJsonData: "/formDesign/formDesign/queryByCode"
+        loadJsonData: "/formDesign/formDesign/queryByCode",
       }
-    }
-  },
-  watch: {
-    code() {
-      this.loadJsonData()
     }
   },
   created() {
@@ -29,12 +24,31 @@ export default {
   },
   methods: {
     loadJsonData(){
-        getAction(`${this.url.loadJsonData}`,{code: this.code}).then(res=>{
+        getAction(`${this.url.loadJsonData}`,{code: this.config.code}).then(res=>{
           if(res.success){
             this.jsonData = JSON.parse(res.result.designData)
+            this.loadFormData()
           }else{
             this.$message.warning(res.message)
           }
+        })
+    },
+    loadFormData(){
+      getAction(`${this.config.url.loadFormData}`).then(res=>{
+        if(res.success){
+          this.$refs.kfb.setData(res.result)
+        }else{
+          this.$message.warning(res.message)
+        }
+      })
+    },
+    submitData (getData) {
+      getData()
+        .then(values => {
+          console.log('验证通过', values)
+        })
+        .catch(() => {
+          console.log('验证未通过，获取失败')
         })
     }
   }
