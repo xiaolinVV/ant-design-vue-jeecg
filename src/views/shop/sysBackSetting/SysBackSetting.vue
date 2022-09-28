53a6ac0edbb4cab4ec418a0e725676b49dc8da2f
张少林 <1083065285@qq.com>
2022-09-28 23:14:45 +0800

    feat：后台设置

---
+++ src/views/shop/sysBackSetting/SysBackSetting.vue
@@ -0,0 +1,27 @@
+<template>
+  <form-build-page :config='config'></form-build-page>
+</template>
+
+<script>
+import FormBuildPage from '@comp/designForm/FormBuildPage'
+export default {
+  name: 'SysBackSetting',
+  components:{
+    FormBuildPage
+  },
+  data() {
+    return {
+      config: {
+        code: "background_setting",
+        url: {
+          loadFormData: "/sysBackSetting/sysBackSetting/queryFirstData"
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
