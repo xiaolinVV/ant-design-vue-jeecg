fe605313050363956b2bf6d915d1bfcbacafd6ef
张少林 <1083065285@qq.com>
2023-06-03 11:24:53 +0800

    feat：签名密钥串(前后端要一致，正式发布请自行修改)

--- src/utils/encryption/signMd5Utils.js
+++ src/utils/encryption/signMd5Utils.js
@@ -1,6 +1,6 @@
 import md5 from 'md5'
 //签名密钥串(前后端要一致，正式发布请自行修改)
-const signatureSecret = "tszpjifjtu7gz2kmh8qlk58a6mzjj736";
+const signatureSecret = "EhsCbgOKELQsnoz4UJLWxgoN";
 
 export default class signMd5Utils {
   /**
