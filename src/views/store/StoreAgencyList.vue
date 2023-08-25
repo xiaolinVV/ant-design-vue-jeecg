96bfa9cd3b1ec9b0be2b1d8c165c600c8e1e3dfd
张少林 <1083065285@qq.com>
2023-08-25 18:04:41 +0800

    feat：店铺代理商列表

---
+++ src/views/store/StoreAgencyList.vue
@@ -0,0 +1,279 @@
+<template>
+  <a-row :gutter="10">
+    <a-col :span="6">
+      <!-- <a-card :bordered="false" style="min-height: 800px">
+        <a-input-search style="margin-bottom: 8px" placeholder="店铺查询" @change="onChange" />
+
+        <a-tree v-if="storeList.length > 0" :tree-data="storeListFilt" @select="getSelectStore"> </a-tree>
+      </a-card> -->
+      <a-card title="店铺列表" :bordered="false" style="min-height: 1000px">
+        <store-tree @ok="getSelectStore"></store-tree>
+      </a-card>
+    </a-col>
+    <a-col :span="18">
+      <a-card :bordered="false" style="width: 100%;min-height: 800px">
+        <!-- 查询区域 -->
+        <!-- <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="24">
+
+          <a-col :md="6" :sm="8">
+            <a-form-item label="创建日">
+              <a-input placeholder="请输入创建日" v-model="queryParam.day"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="8">
+            <a-form-item label="删除状态（0，正常，1已删除）">
+              <a-input placeholder="请输入删除状态（0，正常，1已删除）" v-model="queryParam.delFlag"></a-input>
+            </a-form-item>
+          </a-col>
+        <template v-if="toggleSearchStatus">
+        <a-col :md="6" :sm="8">
+            <a-form-item label="会员列表id">
+              <a-input placeholder="请输入会员列表id" v-model="queryParam.memberListId"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="8">
+            <a-form-item label="创建月">
+              <a-input placeholder="请输入创建月" v-model="queryParam.month"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="8">
+            <a-form-item label="编号">
+              <a-input placeholder="请输入编号" v-model="queryParam.serialnumber"></a-input>
+            </a-form-item>
+          </a-col>
+          </template>
+          <a-col :md="6" :sm="8" >
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+              <a @click="handleToggleSearch" style="margin-left: 8px">
+                {{ toggleSearchStatus ? '收起' : '展开' }}
+                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
+              </a>
+            </span>
+          </a-col>
+
+        </a-row>
+      </a-form>
+    </div>-->
+
+        <!-- 操作按钮区域 -->
+        <div class="table-operator">
+          <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+          <a-button type="primary" icon="download"  :loading="exportLoading"  @click="handleExportXls('店铺代理商')">导出</a-button>
+          <a-upload
+            name="file"
+            :showUploadList="false"
+            :multiple="false"
+            :headers="tokenHeader"
+            :action="importExcelUrl"
+            @change="handleImportExcel"
+          >
+            <a-button type="primary" icon="import">导入</a-button>
+          </a-upload>
+          <a-dropdown v-if="selectedRowKeys.length > 0">
+            <a-menu slot="overlay">
+              <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
+            </a-menu>
+            <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down"/></a-button>
+          </a-dropdown>
+        </div>
+
+        <!-- table区域-begin -->
+        <div>
+          <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+            <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择
+            <a style="font-weight: 600">{{ selectedRowKeys.length }}</a
+            >项
+            <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+          </div>
+
+          <a-table
+            ref="table"
+            size="middle"
+            bordered
+            rowKey="id"
+            :scroll="{ x: 1000 }"
+            :columns="columns"
+            :dataSource="dataSource"
+            :pagination="ipagination"
+            :loading="loading"
+            :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+            @change="handleTableChange"
+          >
+            <span slot="agencyType" slot-scope="text, record">
+              {{ franchiserTypeText(record) }}
+            </span>
+            <span slot="action" slot-scope="text, record">
+              <a @click="handleEdit(record)">编辑</a>
+
+              <a-divider type="vertical" />
+              <a-dropdown>
+                <a class="ant-dropdown-link">更多 <a-icon type="down"/></a>
+                <a-menu slot="overlay">
+                  <a-menu-item>
+                    <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                      <a>删除</a>
+                    </a-popconfirm>
+                  </a-menu-item>
+                </a-menu>
+              </a-dropdown>
+            </span>
+          </a-table>
+        </div>
+        <!-- table区域-end -->
+
+        <!-- 表单区域 -->
+        <storeAgency-modal ref="modalForm" @ok="modalFormOk"></storeAgency-modal>
+      </a-card>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+import StoreAgencyModal from './modules/StoreAgencyModal'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import { getAction } from '@/api/manage'
+import StoreTree from '../common/StoreTree/StoreTree'
+
+export default {
+  name: 'StoreAgencyList',
+  mixins: [JeecgListMixin],
+  components: {
+    StoreAgencyModal,
+    StoreTree,
+  },
+  data() {
+    return {
+      description: '店铺代理商管理页面',
+      // 表头
+      columns: [
+        {
+          title: '#',
+          dataIndex: '',
+          key: 'rowIndex',
+          width: 60,
+          align: 'center',
+          customRender: function(t, r, index) {
+            return parseInt(index) + 1
+          }
+        },
+        {
+          title: '编号',
+          align: 'center',
+          dataIndex: 'serialnumber'
+        },
+        {
+          title: '会员账号',
+          align: 'center',
+          dataIndex: 'memberList.phone'
+        },
+        {
+          title: '会员昵称',
+          align: 'center',
+          dataIndex: 'memberList.nickName'
+        },
+        {
+          title: '代理商类型',
+          align: 'center',
+          dataIndex: 'memberList.franchiserType',
+          scopedSlots: { customRender: 'agencyType' }
+        },
+
+        {
+          title: '创建时间',
+          align: 'center',
+          dataIndex: 'createTime'
+        },
+        {
+          title: '创建者',
+          align: 'center',
+          dataIndex: 'createBy'
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          align: 'center',
+          scopedSlots: { customRender: 'action' }
+        }
+      ],
+      url: {
+        list: '',
+        delete: '/storeAgency/storeAgency/delete',
+        deleteBatch: '/storeAgency/storeAgency/deleteBatch',
+        exportXlsUrl: '/storeAgency/storeAgency/exportXls',
+        importExcelUrl: '/storeAgency/storeAgency/importExcel',
+        getAllStoreList: 'storeManage/storeManage/getAllStoreList'
+      },
+      storeList: [],
+      storeListFilt: [],
+      storeId: '',
+      disableMixinCreated: true
+    }
+  },
+  computed: {
+    importExcelUrl: function() {
+      return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`
+    }
+  },
+  created() {
+    this.getAllStoreList()
+  },
+  methods: {
+    franchiserTypeText(record) {
+      let text = ''
+      switch (record.agencyType * 1) {
+        case 0:
+          text = '店铺批发产品代理商'
+          break
+      }
+      return text
+    },
+    handleAdd() {
+      if (!this.storeId) {
+        this.$message.warning('请选择添加的商户')
+        return
+      }
+      this.$refs.modalForm.add({ storeId: this.storeId })
+    },
+    onChange(e) {
+      console.log(e.data)
+      if (e.data) {
+        this.storeListFilt = this.storeList.filter(item => item.title.indexOf(e.data) > -1)
+      } else {
+        this.storeListFilt = this.storeList
+      }
+    },
+    getSelectStore(e) {
+      console.log(e)
+      this.storeId = e.key
+      this.url.list = '/storeAgency/storeAgency/list?storeManageId=' + e.key
+      this.modalFormOk()
+    },
+    getAllStoreList() {
+      getAction(this.url.getAllStoreList, {}).then(res => {
+        if (res.success) {
+          for (let s of res.result) {
+            this.storeList.push({
+              title: s.storeName,
+              key: s.id
+            })
+            this.storeListFilt.push({
+              title: s.storeName,
+              key: s.id
+            })
+          }
+          console.log(this.storeList)
+        } else {
+          this.$message.warning(res.message)
+        }
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+@import '~@assets/less/common.less';
+</style>
